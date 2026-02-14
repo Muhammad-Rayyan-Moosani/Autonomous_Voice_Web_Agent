7994999a98a3443d26e6c928c7d4bbafc40b72ed
Muhammad-Rayyan-Moosani <mrayyanm411@gmail.com>
2026-02-14 16:35:18 -0500

    some additional files

--- backend/app.py
+++ backend/app.py
@@ -3,3 +3,14 @@
 # Initializes server, CORS, and routes
 # Handles POST requests from extension
 # Connects to AI planner and services
+
+from flask import Flask, jasonify, request
+from flask_cors import CORS
+from  dotenv import load_dotenv
+import os
+load_dotenv()
+
+
+app = Flask(__name__)
+CORS(app)
+
