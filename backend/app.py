30791f30cc17724a3c44a40f05af75e3555e466e
Muhammad-Rayyan-Moosani <mrayyanm411@gmail.com>
2026-02-14 21:20:14 -0500

    gg

--- backend/app.py
+++ backend/app.py
@@ -12,3 +12,22 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/test", methods=['POST'])
+def test():
+    data = request.json
+    transcript = data.get("transcript ", "")
+    page_context = data.get("page_context", {})
+
+    response = {
+        "intent":"demo",
+        "response":f'Received transcript: {transcript}',
+        "page_context": page_context
+    }
+    return jsonify(response)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
